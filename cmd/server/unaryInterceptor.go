5639edd69b16a25682876d6f5797ec62fe41e893
Exzrgs <s.nakao0120@gmail.com>
2023-10-27 14:14:26 +0900

    複数のユーナリーインターセプターを実装

--- cmd/server/unaryInterceptor.go
+++ cmd/server/unaryInterceptor.go
@@ -13,3 +13,10 @@ func myUnaryServerInterceptor1(ctx context.Context, req any, info *grpc.UnarySer
 	log.Println("[post] my unary server interceptor 1: ", res)
 	return res, err
 }
+
+func myUnaryServerInterceptor2(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
+	log.Println("[pre] my unary server interceptor 2: ", req)
+	res, err := handler(ctx, req)
+	log.Println("[post] my unary server interceptor 2: ", res)
+	return res, err
+}
